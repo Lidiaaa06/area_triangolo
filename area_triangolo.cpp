19ed1855a495f7c3e8ca5ced0a6213428bea6e3f
Lidiaaa06 <113819620+Lidiaaa06@users.noreply.github.com>
2022-09-26 10:01:35 +0200

    Add files via upload

---
+++ area_triangolo.cpp
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+int main ()
+{
+int base,altezza;
+int area;
+printf (" insertisci il valore della base: ");
+scanf("%d",&base);
+printf("inserisci il valore dell altezza: ");
+scanf("%d",&altezza);
+area=base*altezza/2;
+printf ("area= \t%d",area);
+return 0;	
+}
